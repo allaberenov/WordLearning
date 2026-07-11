5d407e003372459daf43966fa5668199f64df0ec
Kerim <allaberenov.kerim.21@gmail.com>
2026-07-11 21:43:37 +0300

    fix tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vitest/config";
+import { fileURLToPath } from "node:url";
 
 export default defineConfig({
   test: {
@@ -8,7 +9,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": "/home/kerim/Documents/projects/word_learning/src"
+      "@": fileURLToPath(new URL("./src", import.meta.url))
     }
   }
 });
